92809392ac2a3d49d3152aabfae23c5fc580105b
anishsri19 <siranish97@gmail.com>
2022-04-18 00:27:48 +0530

    tree node insertion

---
+++ Tree/treeNode.java
@@ -0,0 +1,14 @@
+
+
+// tree node structure
+class treeNode
+{
+    int value;
+    treeNode left,right;
+    treeNode(int value)
+    {
+        this.value=value;
+        this.left=null;
+        this.right=null;
+    }
+}
